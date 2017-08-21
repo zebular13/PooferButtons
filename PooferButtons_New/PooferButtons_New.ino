db6bd96dc78257f8f740fdf68a5466494dac5447
Monica Houston <monica.s.houston@gmail.com>
2017-08-20 17:00:20 -0700

    used PROGMEM for large global variables

--- PooferButtons_New/PooferButtons_New.ino
+++ PooferButtons_New/PooferButtons_New.ino
@@ -1,11 +1,12 @@
 #include <Bounce.h>
+#include <avr/pgmspace.h>
 
 class Poofer
 {
     //CLass Member Variables
     //These are initialized at startup
     int pooferPin; // the number of Poofer pins
-    unsigned long OnTime; // milliseconds of on-time
+    unsigned int OnTime; // milliseconds of on-time
     elapsedMillis elapsedOn;
 
     //these maintain the current state
@@ -107,28 +108,12 @@ Poofer poofBig1(out4);
 Poofer poofBig2(out5);
 Poofer poofBig3(out6);
 
-void setup() {
-  Serial1.begin(9600);
-  Serial.begin(9600);
 
-  pinMode(1, INPUT_PULLUP);
-  pinMode(2, INPUT_PULLUP);
-  pinMode(3, INPUT_PULLUP);
 
-  pinMode(out1, OUTPUT);
-  pinMode(out2, OUTPUT);
-  pinMode(out3, OUTPUT);
-
-  pinMode(out4, OUTPUT);
-  pinMode(out5, OUTPUT);
-  pinMode(out6, OUTPUT);
-}
-
-//code to play Moopi's fire patterns
-//const int tempo
+//Moopi's fire patterns
 
 //the choo choo train
-const int pattern0[] = {
+const int pattern0[] PROGMEM = {
   1, 500, 0, 510,
   2, 500, 0, 510, 
   3, 500, 0, 510,
@@ -148,7 +133,7 @@ const int pattern0[] = {
   2, 500, 0, 510,
   3, 500, 0, 510,
   4, 500, 0, 510,
-  
+  //faster!
   2, 250, 0, 260,
   3, 250, 0, 260,
   2, 250, 0, 260,
@@ -168,7 +153,7 @@ const int pattern0[] = {
   3, 250, 0, 260,
   1, 250, 0, 260,
   4, 250, 0, 260,
-  
+  //faster and faster!
   3, 120, 0, 130,
   1, 120, 0, 130,
   2, 120, 0, 130,
@@ -183,14 +168,14 @@ const int pattern0[] = {
   3, 120, 0, 130,
   1, 120, 0, 130,
   5, 250, 0, 630,
-
+ //BOOM!
   1, 1000, 
   2, 1000, 
   3, 1000, 
   4, 1000, 
   5, 1000, 
   6, 1000,  0, 1600,
-
+ //BOOM!
   1, 1000, 
   2, 1000, 
   3, 1000, 
@@ -199,15 +184,16 @@ const int pattern0[] = {
   6, 1000,
 };
 ///pop goes the weasel
-const int pattern1[] = {
+const int pattern1[] PROGMEM = {
   //Round and round the mulberry bush 
   2, 500, 0, 510,
   3, 500, 0, 510,
-  1, 500, 0, 510,
+  1, 500, 0, 610,
+  
   2, 500, 0, 510,
- 
   3, 250, 0, 260,
   4, 250, 0, 260,
+  
   2, 250, 0, 260,
   3, 500, 0, 810, 
   //the monkey chased the weasel 
@@ -225,7 +211,7 @@ const int pattern1[] = {
   //the monkey thought 'twas all in good fun
   2, 500, 0, 510,
   3, 250, 0, 260,
-  1, 250, 0, 260,
+  1, 250, 0, 360,
   
   2, 250, 0, 260,
   3, 250, 0, 260,
@@ -234,15 +220,16 @@ const int pattern1[] = {
   
   3, 250, 0, 260,
   1, 500, 0, 810,
-  //POP! Goes the weasel (plus surprise burst) 
+  //POP! 
   1, 1000, 
   2, 1000, 
   3, 1000, 
   4, 1000, 
   5, 1000, 
   6, 1000,  0, 1500,
-
-  2, 250, 0, 260,
+//Goes 
+  2, 250, 0, 360,
+  //the weasel
   3, 250, 0, 260,
   1, 250, 0, 260,
   2, 250, 0, 1250,
@@ -255,7 +242,8 @@ const int pattern1[] = {
   6, 1000,  
 };
 //Safety Dance/ Shave and a Haircut 
-const int pattern2[] = {
+const int pattern2[] PROGMEM = {
+  //safety dance
   3, 500, 0, 510,
   3, 500, 0, 510,
   2, 500, 0, 510,
@@ -286,47 +274,33 @@ const int pattern2[] = {
   3, 250, 0, 260,
   3, 250, 0, 760,
 
-  1, 250, 0, 260,
-  2, 120, 0, 130,
-  3, 250, 0, 260,
-  3, 250, 0, 260,
-  3, 250, 0, 260,
-  1, 120, 0, 130, 
-  2, 250, 0, 760,
-
-  //burst
-  1, 1000, 
-  2, 1000, 
-  3, 1000, 
-  4, 1000, 
-  5, 1000, 
-  6, 1000, 0, 2000,
+  //shave and a haircut
+  1, 100, 0, 250,
+  1, 100, 0, 150,
   
- //fire dance
-  3, 500, 0, 510,
-  3, 250, 0, 260,
-  3, 500, 0, 510,
-  1, 120, 0, 130,
-  1, 120, 0, 630,
+  1, 100, 0, 150,
+  4, 100, 0, 400,
+  
+  1, 100, 0, 700,
   
   //burst
-  1, 1000, 
-  2, 1000, 
-  3, 1000, 
-  4, 1000, 
-  5, 1000, 
-  6, 1000, 0, 1110,
+  1, 100, 
+  2, 100, 
+  3, 100, 
+  4, 100, 
+  5, 100, 
+  6, 100, 0, 300,
 
   //burst
-  1, 1000, 
-  2, 1000, 
-  3, 1000, 
-  4, 1000, 
-  5, 1000, 
-  6, 1000
+  1, 80,
+  2, 80,
+  3, 80,
+  4, 80,
+  5, 80,
+  6, 80, 0, 280
 };
 //Ragg Mopp
-const int pattern3[] = {
+const int pattern3[] PROGMEM = {
   //ragg mopp!
   1, 500,
   2, 500,
@@ -456,75 +430,168 @@ const int pattern3[] = {
   3, 250,
   4, 250,
   5, 250,
-  6, 250, 0, 260
-  
-}
+  6, 250, 0, 260 
+};
 //Mexican Hat Dance 
-const int pattern4[] = {
-  1,
-  1,
-  2,
-  3,
-  2,
-  3,
-  2,
-  1,
-  4,
-  3,
-  0,
-  3,
-  3,
-  1,
-  2,
-  1,
-  2,
-  1,
-  3,
-  1,
-  4,
-  
-  0,
-  
-  2,
-  2,
-  3,
-  1,
-  3,
-  1,
-  3,
-  2,
-  6,
-  1,
-  
-  0,
-  1,
-  1,
-  2,
-  2,
-  3,
-  3,
-  3,
-  2,
-  1,
-  0,
-  3,
-
+const int pattern4[] PROGMEM = {
+  //  Di-di di-dee-dee-di-dee, 
+  1, 120, 0, 130,
+  1, 120, 0, 130,
+  2, 250, 0, 260,
+  3, 250, 0, 260,
+  2, 250, 0, 260,
+  3, 250, 0, 260,
+  2, 250, 0, 260,
+  1, 120, 0, 130,
+  4, 250, 0, 260,
+  3, 500, 0, 1010,
+//  Di-di di-dee-dee-di-doo,  
+  3, 120, 0, 130,
+  3, 120, 0, 130,
+  1, 250, 0, 260,
+  2, 250, 0, 260,
+  1, 250, 0, 260,
+  2, 250, 0, 260,
+  1, 250, 0, 260,
+  3, 120, 0, 130,
+  1, 250, 0, 260,
+  4, 500, 0, 1010,
+  //  Di-di di-dee-dee-di-dee, 
+  2, 120, 0, 130,
+  2, 120, 0, 130,
+  3, 250, 0, 260,
+  1, 250, 0, 260,
+  3, 250, 0, 260,
+  1, 250, 0, 260,
+  3, 250, 0, 260,
+  2, 120, 0, 130,
+  6, 250, 0, 260,
+  1, 500, 0, 510,
+  //  Dee-di-dee-dee di dee-
+  1, 250, 0, 260,
+  1, 120, 0, 130,
+  2, 250, 0, 260,
+  2, 120, 0, 130,
+  3, 250, 0, 260,
+  3, 250, 0, 260,
+  3, 120, 0, 130,
+  2, 250, 0, 260,
+  1, 500, 0, 1010,
+//pause
+  3, 250, 0, 260,
+//O-LE!
   1, 1000, 
   2, 1000, 
   3, 1000, 
   4, 1000, 
   5, 1000, 
   6, 1000
-
-}
+};
 //Wipe Out 
-const int pattern5[] = {
+const int pattern5[] PROGMEM = {
+  2, 120, 0, 130,
+  3, 250, 0, 260,
+  3, 120, 0, 230, //pause
+  
+  1, 250, 0, 260,
+  2, 250, 0, 260,
+  3, 250, 0, 260,
+  
+  4, 250, 0, 260,
+  4, 120, 0, 130,
+  3, 120, 0, 130,
+  1, 250, 0, 260,
+  1, 120, 0, 230, //pause
+  
+  2, 250, 0, 260,
+  2, 250, 0, 260,
+  3, 250, 0, 260,
+  
+  1, 250, 0, 260,
+  5, 250, 0, 260,
+  5, 120, 0, 130,
+  
+  6, 120, 0, 130,
+  4, 250, 0, 260,
+  4, 120, 0, 230, //pause
+  
+  5, 250, 0, 260,
+  6, 250, 0, 260,
+  4, 250, 0, 260,
+  
+  2, 250, 0, 260,
+  2, 120, 0, 130,
+  4, 120, 0, 130,
+  
+  5, 250, 0, 260,
+  5, 250, 0, 360, //pause
+  
+  6, 250, 0, 260,
+  4, 250, 0, 260,
+  5, 250, 0, 260,
+  3, 250, 0, 260,
+  3, 120, 0, 130,
+  
+  1, 120, 0, 130,
+  2, 250, 0, 260,
+  2, 120, 0, 230, //pause
+  
+  3, 250, 0, 260,
+  3, 250, 0, 260,
+  1, 250, 0, 260,
+  5, 250, 0, 260,
+  5, 120, 0, 130,
+  5, 120, 0, 130,
+  5, 120, 0, 130,
 
-}
-//Personal Jesus 
-const int pattern6[] = {
+  1, 250,
+  2, 250,
+  3, 250,
+  4, 250,
+  5, 250,
+  6, 250, 0, 260,
 
-}
+  1, 250,
+  2, 250,
+  3, 250,
+  4, 250,
+  5, 250,
+  6, 250, 0, 260,
+
+  1, 500, 
+  2, 500, 
+  3, 500, 
+  4, 500, 
+  5, 500, 
+  6, 500, 0, 1500,
+  //RANDOM
+  1, 120, 0, 121,
+  1, 100,
+  4, 120,
+  1, 80, 0, 131,
+
+  1, 90,
+  2, 60,
+  3, 120,
+  4, 160,
+  5, 120,
+  6, 180, 0, 190,
+
+  1, 90,
+  2, 60,
+  3, 120,
+  4, 160,
+  5, 120,
+  6, 180, 0, 201,
+  3, 200, 0, 211,
+  5, 100,
+  4, 120,
+  1, 80, 0, 91,
+
+  1, 150, 0, 150,
+  6, 120, 0, 180,
+  3, 40, 0, 40
+};
 
 const int * listOfPatterns[] = {
   pattern0,
@@ -532,17 +599,15 @@ const int * listOfPatterns[] = {
   pattern2,
   pattern3,
   pattern4, 
-  pattern5,
-  pattern6
+  pattern5
 };
 const int patternLengths[] = {
-  sizeof(pattern0) / sizeof(int),
+  sizeof(pattern0) / sizeof(int), //do I need to get these using progmem?
   sizeof(pattern1) / sizeof(int),
   sizeof(pattern2) / sizeof(int),
   sizeof(pattern3) / sizeof(int),
   sizeof(pattern4) / sizeof(int),
-  sizeof(pattern5) / sizeof(int),
-  sizeof(pattern6) / sizeof(int)
+  sizeof(pattern5) / sizeof(int)
 };
 const int numberOfPatterns = sizeof(listOfPatterns) / sizeof(const int *);
 int pattern_position;
@@ -559,13 +624,13 @@ void start_pattern(int num) //the function that gets called to start the pattern
 }
 
 void update_pattern() //runs in the loop
-{
+{ 
   if (whichPattern < numberOfPatterns) {
     const int * pattern = listOfPatterns[whichPattern];
     int patternlength = patternLengths[whichPattern];
 
-    int poofer = pattern[pattern_position];
-    unsigned int duration = pattern[pattern_position + 1];
+    int poofer = pgm_read_word(pattern + pattern_position); // use PROGMEM to read the number of poofer being selected
+    unsigned int duration = pgm_read_word(pattern + (pattern_position + 1)); //
 
     if (poofer == 0) { // if the poofer is zero, create a musical rest or delay
       if (delayStarted == false) {
@@ -609,7 +674,24 @@ void poofer_on(int num, int duration) //translates the array numbers to the corr
     poofBig3.On(duration);
   }
 }
+void setup() {
+  Serial1.begin(9600);
+  Serial.begin(9600);
 
+  pinMode(1, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+
+  pinMode(out1, OUTPUT);
+  pinMode(out2, OUTPUT);
+  pinMode(out3, OUTPUT);
+
+  pinMode(out4, OUTPUT);
+  pinMode(out5, OUTPUT);
+  pinMode(out6, OUTPUT);
+
+  Serial.println(patternLengths[0]);
+}
 void loop() {
 
   btn1.update();
@@ -767,7 +849,7 @@ void loop() {
   //pattern buttons
   if (btn1.fallingEdge())
   {
-    start_pattern(0);
+    start_pattern(5);
     Serial.println("btn 1 pressed");
   }
   //pattern buttons
